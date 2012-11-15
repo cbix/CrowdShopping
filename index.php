608c0491726bc080f1a72dfc74832016fe4f4df4
R3curve <david.hellmanns@rwth-aachen.de>
2012-11-15 02:49:54 +0100

    -

--- index.php
+++ index.php
@@ -10,10 +10,21 @@ $tpl->loadTemplatefile("index.html", true, true);
 			$tpl->setCurrentBlock('latest_question');
 				$tpl->setVariable("TITLE", $question->getTitle());
 				$tpl->setVariable("TEXT", $question->getDescription());
-			$tpl->parse("latest_question");
+			$tpl->parseCurrentBlock("latest_question");
 		}
 		$tpl->parse();
-		$tpl -> loadTemplateFile("latest_questions.html");
-	$tpl->parse("content");
+		$tpl->show();
+	$tpl->loadTemplateFile("index.html");
+	$tpl->setCurrentBlock("content");
+	$tpl->parseCurrentBlock("content");
+		$bestUsers = User::getFourBestUser();
+		foreach($bestUsers as $user)
+		{
+			$tpl->setCurrentBlock("best_users");
+				$tpl->setVariable("USERNAME", $user->getName());
+				$tpl->setVariable("GRADE", $user->getRank());
+			$tpl->parseCurrentBlock("best_users");
+		}	
 
 $tpl->show();
+?>
